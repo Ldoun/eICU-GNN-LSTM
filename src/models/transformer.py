89774ea717e89c353234ab3f0ed1a104df545b36
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-08-22 16:22:26 -0700

    feat: add custome en_layer to save attention&grad

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from src.models.layer import TransformerEncoderLayer
 from src.models.utils import init_weights, get_act_fn, trunc_normal_
 import math
 
@@ -30,33 +31,8 @@ class TimeSeriesTransformer(nn.Module):
         if config['transformer_pooling']== 'first':
             self.use_cls = True
             self.cls_token = nn.Parameter(torch.zeros(1, 1, config['feature_size'])) # not using trunc_normal
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=config['feature_size'], nhead=config['n_head'], dropout=config['transformer_dropout']) #batch_first=True not possible
+        self.encoder_layer = TransformerEncoderLayer(d_model=config['feature_size'], nhead=config['n_head'], dropout=config['transformer_dropout']) #batch_first=True not possible
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=config['num_layers'])
-        
-        if config['fp_attn_transformer']:
-            self.attn_gradients = {}
-            self.attention_map = {}
-
-            for n, encoder_layer in enumerate(self.transformer_encoder.layers):
-                encoder_layer.self_attn.register_forward_hook(self.save_attention_map(n))
-
-    def save_attn_gradients(self, n):
-        def fn(attn_gradients):
-            self.attn_gradients[n] = attn_gradients
-        return fn
-
-    def get_attn_gradients(self):
-        return self.attn_gradients
-
-    def save_attention_map(self, n):
-        def fn(_, __, output):
-            output[1].register_hook(self.save_attn_gradients(n))
-            self.attention_map[n] = output[1]
-            return output[0],output[1]
-        return fn
-
-    def get_attention_map(self):
-        return self.attention_map
                 
     def forward(self, src):
         if self.use_cls:
