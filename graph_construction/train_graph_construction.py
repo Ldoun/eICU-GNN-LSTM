c88425efae1543f5f2b9586a3cd3a8941fe829d7
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-08-23 23:40:49 -0700

    fix: seed everything

--- graph_construction/train_graph_construction.py
+++ graph_construction/train_graph_construction.py
@@ -104,9 +104,22 @@ def get_dataloader():
     #valid_loader = DataLoader(dataset=valid_dataset, batch_size=2, shuffle=False)
     return train_loader, Los_data
 
+def seed_everything(seed: int):
+    import random, os
+    import numpy as np
+    import torch
+    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
 
 
 if __name__ == '__main__':
+    seed_everything(42)
     trainer = Trainer()
     trainer.train()
 
