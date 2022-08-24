2b73d6051726aa38bf4ed83015d486b78dc77411
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-08-23 17:12:12 -0700

    fix: add alpha beta gamma log

--- make_graph_with_multiprocess.py
+++ make_graph_with_multiprocess.py
@@ -16,6 +16,7 @@ for _ in range(n_trial // once_per_trial):
         config['alpha'] = random.randrange(0,100)
         config['beta'] = random.randrange(0,100)
         config['gamma'] = random.randrange(-100,0)
+        print(str(config['alpha']) + str(config['beta']) + str(config['gamma']))
         write_file.write(str(config['alpha']) + str(config['beta']) + str(config['gamma']) +'\n')
         p = mp.Process(target=get_graph,kwargs=config)
         p.start()
